4411f5effe5581b84e62f59d71d490e862afd864
Pixelz123 <souptik4444@gmail.com>
2026-06-02 09:03:22 +0530

    initial commit

---
+++ src/test/java/com/prodrag/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodrag.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
